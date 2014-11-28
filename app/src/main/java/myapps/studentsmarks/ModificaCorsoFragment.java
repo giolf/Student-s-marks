c2ce6d22d96b355d5e498c14a19fef57f2710f5e
Giovanni Far <giovanni.far@student.supsi.ch>
2014-11-28 23:50:38 +0100

    implementazione - modifica corso

--- app/src/main/java/myapps/studentsmarks/ModificaCorsoFragment.java
+++ app/src/main/java/myapps/studentsmarks/ModificaCorsoFragment.java
@@ -4,16 +4,23 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.Fragment;
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import static myapps.studentsmarks.Utility.customTitleDialog;
 
 /**
  * Created by Gio on 16.11.2014.
@@ -41,106 +48,222 @@ public class ModificaCorsoFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_modifica_corso, container, false);
-        final StudentMarks activity = (StudentMarks)getActivity();
+        final StudentMarks activity = ((StudentMarks)getActivity());
+        final Button btnSalva       = (Button)rootView.findViewById(R.id.salva);
+        final Button btnAnno        = (Button)rootView.findViewById(R.id.anno);
+        final Button btnCorso       = (Button)rootView.findViewById(R.id.corso);
+        final Button btnCorsoDM     = (Button)rootView.findViewById(R.id.corso_dm);
+        final TextView tvAnno      = (TextView)rootView.findViewById(R.id.tv_anno);
+        final TextView tvCorso      = (TextView)rootView.findViewById(R.id.tv_corso);
+        final TextView tvCorsoDM    = (TextView)rootView.findViewById(R.id.tv_corso_dm);
+        final TextView tvlCorso     = (TextView)rootView.findViewById(R.id.tvl_corso);
+        final TextView tvlCorsoDM   = (TextView)rootView.findViewById(R.id.tvl_corso_dm);
 
-        //setup del dialog
-        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
-        builder.setCancelable(false);
-        final DatePicker picker = new DatePicker(activity);
-        picker.setDescendantFocusability(DatePicker.FOCUS_BLOCK_DESCENDANTS);
-        picker.setCalendarViewShown(false);
-        builder.setView(picker);
-
-        //titolo dialog
-        //builder.setCustomTitle( customTitleDialog(activity, R.string.dialog_tit_mc, R.string.dialog_stit_mc, 0) );
-
-        //listeners bottoni 'annulla' e 'seleziona'
-        final Dialog.OnClickListener annulla = new Dialog.OnClickListener() {
+        btnAnno.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                //riporto l'utente alla voce del menu 'riepilogo annuale'
-                activity.getMNavigationDrawerFragment().selectItem(0);
-                activity.onSectionAttached(1);
-                activity.restoreActionBar();
-                activity.getMenu().findItem(R.id.action_selected_year).setVisible(true);
-                activity.getMenu().findItem(R.id.action_settings).setVisible(true);
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch(motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        btnAnno.setBackgroundColor(Color.parseColor("#e6e6e6"));
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        btnAnno.setBackgroundColor(Color.parseColor("#ffffff"));
+
+                        //Mostro il Dialog che permette la scelta dell'anno del corso
+                        //setup del dialog
+                        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+                        builder.setCancelable(false);
+                        final DatePicker picker = new DatePicker(activity);
+                        picker.setDescendantFocusability(DatePicker.FOCUS_BLOCK_DESCENDANTS);
+                        picker.setCalendarViewShown(false);
+                        builder.setView(picker);
+
+                        //titolo dialog
+                        builder.setCustomTitle( customTitleDialog(activity, R.string.dialog_tit_cc) );
+
+                        //listener bottone 'seleziona'
+                        Dialog.OnClickListener seleziona = new Dialog.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                 /*Se l'utente cambia anno DOPO aver gia scelto un corso, deve rifare tutta la procedura
+                                  *questo avviene perche non è detto che il corso selezionato nell'anno precedente ci sia
+                                  *anche nell'anno appena selezionato
+                                  */
+                                if( !tvCorso.getText().equals(getResources().getString(R.string.layout_msg_mc)) ) {
+                                    //resetto il fragment per renderlo disponibile per la modifica di un nuovo corso
+                                    tvAnno.setText(getResources().getText(R.string.layout_msg_cc));
+                                    tvCorso.setText(getResources().getString(R.string.layout_msg_mc));
+                                    tvCorso.setTextColor(Color.parseColor("#ffffff"));
+                                    tvlCorso.setTextColor(Color.parseColor("#ffffff"));
+                                    btnCorso.setBackgroundColor(Color.parseColor("#dedede"));
+                                    btnCorso.setEnabled(false);
+                                    tvCorsoDM.setText(getResources().getString(R.string.layout_msg_mc2));
+                                    tvCorsoDM.setTextColor(Color.parseColor("#ffffff"));
+                                    tvlCorsoDM.setTextColor(Color.parseColor("#ffffff"));
+                                    tvlCorsoDM.setText(getResources().getString(R.string.layout_tvl_corso_dm));
+                                    btnCorsoDM.setBackgroundColor(Color.parseColor("#dedede"));
+                                    btnCorsoDM.setEnabled(false);
+                                    btnSalva.setBackgroundColor(Color.parseColor("#d2d2d2"));
+                                    btnSalva.setEnabled(false);
+                                    //mostro un messaggio che avverte l'utente
+                                    Toast toast = Toast.makeText(activity, getResources().getString(R.string.errore_msg), Toast.LENGTH_LONG);
+                                    TextView tv = (TextView)toast.getView().findViewById(android.R.id.message);
+                                    tv.setGravity(Gravity.CENTER);
+                                    toast.show();
+                                    //esco dal metodo interrompendo il flusso del listener
+                                    return;
+                                }
+                                //mostro l'anno selezionato dall'utente nella TextView dell'anno
+                                tvAnno.setText(""+picker.getYear());
+
+                                //abilito il bottone corso
+                                btnCorso.setBackgroundColor(Color.parseColor("#ffffff"));
+                                tvCorso.setTextColor(Color.parseColor("#000000"));
+                                tvlCorso.setTextColor(Color.parseColor("#000000"));
+                                btnCorso.setEnabled(true);
+                            }
+                        };
+
+                        //set bottoni dialog
+                        builder.setPositiveButton(R.string.dialog_btn_seleziona, seleziona);
+                        builder.setNegativeButton(R.string.dialog_btn_annulla, null);
+
+                        //mostro solo l'anno del datepicker
+                        Utility.showJustYear(picker);
+
+                        //visualizzo il dialog
+                        Dialog dialog = builder.create();
+                        dialog.show();
+
+                        return true;
+                }
+                return false;
             }
-        };
-        Dialog.OnClickListener seleziona = new Dialog.OnClickListener() {
+        });
+        btnCorso.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                //salvo l'anno selezionato dall'utente
-                final int annoSelezionato = picker.getYear();
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch(motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        btnCorso.setBackgroundColor(Color.parseColor("#e6e6e6"));
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        btnCorso.setBackgroundColor(Color.parseColor("#ffffff"));
 
-                //qui prendo i corsi dell'anno selezionato
-                final String[] lcAnnoSelezionato = {"Italiano", "Matematica", "Francese", "Fisica", "Tedesco", "Storia", "Scienze", "Latino", "Storia", "Scienze", "Latino"};
+                        //qui prendo i corsi dell'anno selezionato
+                        final String[] lcAnnoSelezionato = {"Italiano", "Matematica", "Francese", "Fisica", "Tedesco", "Storia", "Scienze", "Latino", "Storia", "Scienze", "Latino"};
 
-                //creo un nuovo Dialog per la selezione del corso da modificare in base all anno scelto
-                AlertDialog.Builder builder = new AlertDialog.Builder(activity);
-                builder.setCancelable(false);
-                //builder.setCustomTitle(customTitleDialog(activity, R.string.dialog_tit_mc, R.string.dialog_stit_mcp2, 0));
-                builder.setItems(lcAnnoSelezionato, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, final int index) {
-                        //prendo il corso da modificare in base alla scelta dell'utente
+                        //Mostro un dialog che permette la scelta del corso da modificare nell'anno selezionato
+                        final AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+                        builder.setCancelable(false);
+                        builder.setCustomTitle(customTitleDialog(activity, R.string.dialog_tit_mc));
+                        builder.setItems(lcAnnoSelezionato, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, final int index) {
+                                //prendo il corso da modificare in base alla scelta dell'utente e lo metto nella TextView del corso
+                                tvCorso.setText(""+lcAnnoSelezionato[index]);
 
-                        //attraverso un nuovo dialog faccio inserire all user il nome del corso da modificare
+                                //abilito il bottone 'modifica corso'
+                                btnCorsoDM.setBackgroundColor(Color.parseColor("#ffffff"));
+                                tvlCorsoDM.setText( getResources().getString(R.string.layout_tvl_corso_dm)+" - "+(tvCorso.getText()).toString().toUpperCase() );
+                                tvCorsoDM.setTextColor(Color.parseColor("#000000"));
+                                tvlCorsoDM.setTextColor(Color.parseColor("#000000"));
+                                btnCorsoDM.setEnabled(true);
+                            }
+                        });
+                        builder.setNegativeButton(R.string.dialog_btn_annulla, null);
+                        Dialog dialog = builder.create();
+                        dialog.show();
+                        //imposto le dimensioni della finestra dell'alertDialog
+                        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+                        lp.copyFrom(dialog.getWindow().getAttributes());
+                        lp.height = 1000;
+                        dialog.getWindow().setAttributes(lp);
+                        return true;
+                }
+                return false;
+            }
+        });
+        btnCorsoDM.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        btnCorsoDM.setBackgroundColor(Color.parseColor("#e6e6e6"));
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        btnCorsoDM.setBackgroundColor(Color.parseColor("#ffffff"));
+                        //Apro un dialog che permette la modifica del corso selezionato
                         AlertDialog.Builder builder = new AlertDialog.Builder(activity);
                         builder.setCancelable(false);
                         final EditText inputModificaCorso = new EditText(activity);
                         builder.setView(inputModificaCorso);
-                        //builder.setCustomTitle( customTitleDialog(activity, R.string.dialog_tit_mc, R.string.dialog_stit_mcp3, 0) );
+
+                        //titolo del Dialog
+                        builder.setCustomTitle(customTitleDialog(activity, R.string.dialog_tit_mc2));
                         builder.setPositiveButton(R.string.dialog_btn_modifica, new Dialog.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialogInterface, int i) {
-                                //controllo l'input del corso immesso e poi lo sostituisco con il vecchio nome del corso scelto dall utente
+                                //inserisco il nuovo nome del corso nella TextView del corso da modificare
+                                tvCorsoDM.setText("" + inputModificaCorso.getText());
 
-                                //inizio output messaggio di modifica corso
-                                Toast toast = Toast.makeText(activity, "Hai modificato il corso: " + lcAnnoSelezionato[index] + "\n ora si chiama: " + inputModificaCorso.getText(), Toast.LENGTH_LONG);
-                                TextView textView = (TextView) toast.getView().findViewById(android.R.id.message);
-                                textView.setGravity(Gravity.CENTER);
-                                toast.show();
-                                //fine output messaggio di modifica
+                                //abilito il bottone salva
+                                btnSalva.setBackgroundColor(Color.parseColor("#87a914"));
+                                btnSalva.setEnabled(true);
 
-                                //e poi lo riporto alla prima voce del menu 'riepilogo annuale'
-                                activity.getMNavigationDrawerFragment().selectItem(0);
-                                activity.onSectionAttached(1);
-                                activity.restoreActionBar();
-                                activity.getMenu().findItem(R.id.action_selected_year).setVisible(true);
-                                activity.getMenu().findItem(R.id.action_settings).setVisible(true);
                             }
                         });
-                        builder.setNegativeButton(R.string.dialog_btn_annulla, annulla);
+                        builder.setNegativeButton(R.string.dialog_btn_annulla, null);
                         Dialog dialog = builder.create();
                         dialog.show();
-                    }
-                });
-
-                builder.setNegativeButton(R.string.dialog_btn_annulla, annulla);
-                Dialog dialog = builder.create();
-                dialog.show();
-                //imposto le dimensioni della finestra dell'alertDialog
-                WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
-                lp.copyFrom(dialog.getWindow().getAttributes());
-                lp.height = 1000;
-                dialog.getWindow().setAttributes(lp);
+                        return true;
+                }
+                return false;
             }
-        };
+        });
+        btnSalva.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch(motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        btnSalva.setBackgroundColor(Color.parseColor("#537719"));
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        btnSalva.setBackgroundColor(Color.parseColor("#d2d2d2"));
+                        //faccio un controllo generale sull'input immesso dall'utente
+                        /*
+                            qui faccio il controllo
+                         */
+                        //Salvo la modifica del corso (per ora ci metto solo un messaggio)
+                        //inizio output messaggio di modifica corso
+                        Toast toast = Toast.makeText(activity, "Hai modificato il corso: " + tvCorso.getText() + "\n ora si chiama: " + tvCorsoDM.getText(), Toast.LENGTH_LONG);
+                        TextView textView = (TextView) toast.getView().findViewById(android.R.id.message);
+                        textView.setGravity(Gravity.CENTER);
+                        toast.show();
+                        //fine output messaggio di modifica
 
-        //set bottoni dialog
-        builder.setPositiveButton(R.string.dialog_btn_seleziona, seleziona);
-        builder.setNegativeButton(R.string.dialog_btn_annulla, annulla);
-
-        //mostro solo l'anno del datepicker
-        Utility.showJustYear(picker);
-
-        //visualizzo il dialog
-        Dialog dialog = builder.create();
-        dialog.show();
-
+                        //resetto il fragment per renderlo disponibile per la modifica di un nuovo corso
+                        tvAnno.setText(getResources().getText(R.string.layout_msg_cc));
+                        tvCorso.setText(getResources().getString(R.string.layout_msg_mc));
+                        tvCorso.setTextColor(Color.parseColor("#ffffff"));
+                        tvlCorso.setTextColor(Color.parseColor("#ffffff"));
+                        btnCorso.setBackgroundColor(Color.parseColor("#dedede"));
+                        btnCorso.setEnabled(false);
+                        tvCorsoDM.setText(getResources().getString(R.string.layout_msg_mc2));
+                        tvCorsoDM.setTextColor(Color.parseColor("#ffffff"));
+                        tvlCorsoDM.setTextColor(Color.parseColor("#ffffff"));
+                        tvlCorsoDM.setText(getResources().getString(R.string.layout_tvl_corso_dm));
+                        btnCorsoDM.setBackgroundColor(Color.parseColor("#dedede"));
+                        btnCorsoDM.setEnabled(false);
+                        btnSalva.setBackgroundColor(Color.parseColor("#d2d2d2"));
+                        btnSalva.setEnabled(false);
+                        return true;
+                }
+                return false;
+            }
+        });
         return rootView;
     }
 }
